c6f8564e48d41d5089a514fc74ee6d07836a5a3d
malakdiana <malakdiana00@gmail.com>
2021-07-12 10:32:19 +0700

    up

--- test.php
+++ test.php
@@ -1,4 +1,6 @@
 <?php
-echo password_hash('123456',PASSWORD_DEFAULT);
-
+//echo password_hash('123456',PASSWORD_DEFAULT);
+require 'connection.php';
+$query    = mysqli_fetch_assoc(mysqli_query($conn, "SELECT * FROM adminonline WHERE (email = 'superadmin' OR username = 'superadmin') "));
+echo $query['id'];
 ?>
